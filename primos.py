d8d4b18bccb3c854c0754f329d210edf5f57b236
lcaldara <74353440+lcaldara@users.noreply.github.com>
2020-11-12 07:25:18 -0300

    Delete primos.py

--- primos.py
+++
@@ -1,9 +0,0 @@
-arquivo = open("primos.txt", "r")
-lista = []
-numeros = []
-for num in arquivo:
-    lista.append(num)
-arquivo.close()
-for num in lista[0].split():
-    numeros.append(num)
-print(numeros)
